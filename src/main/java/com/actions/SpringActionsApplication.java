f15b718deb99020278e789fb4cce045748cd4afe
David Anwar <david.anwar@ifg-life.id>
2024-02-14 16:54:27 +0700

    Initial commit

---
+++ src/main/java/com/actions/SpringActionsApplication.java
@@ -0,0 +1,13 @@
+package com.actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActionsApplication.class, args);
+	}
+
+}
