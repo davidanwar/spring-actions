7561c40cb3a775ccdc88d657b643ae6a23ac76c4
David Anwar <david.anwar@ifg-life.id>
2024-02-14 17:02:53 +0700

    git initial

--- src/main/java/com/actions/SpringActionsApplication.java
+++ src/main/java/com/actions/SpringActionsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringActionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringActionsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringActionsApplication.class, args);
+    }
 
 }
